eefbcdcde7bad90fa68e219a039dc9a0815a9638
Ali Abdelfattah Saad <133794591+aliabdelfatah3@users.noreply.github.com>
2025-09-07 17:36:50 +0300

    strucured folders

---
+++ src/layouts/MainLayout.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+
+function MainLayout() {
+    return <>
+        <Header />
+        <Outlet />
+        <Footer />
+    </ >;
+}
+
+export default MainLayout;
